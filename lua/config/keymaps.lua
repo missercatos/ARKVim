d5e5def3e7c454ee1347f5df6fe4d4a268318160
missercatos <25531541306@nynu.edu.cn>
2026-07-09 17:56:27 +0800

    删除了opencode只保留最基础的编程功能

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
--- -- 文件：~/.config/nvim/lua/config/keymaps.lua
 local keymap = vim.keymap
 
 local function map(mode, lhs, rhs, opts)
@@ -31,61 +30,17 @@ map("n", "<leader>r", function()
   })
 end, { desc = "在浮动终端中编译运行 C++" })
 
--- 在右侧终端中打开 opencode
-map("n", "<leader>to", function()
-  local root = vim.fs.root(0, ".git") or vim.fn.getcwd()
-  Snacks.terminal("opencode", {
-    cwd = root,
-    win = {
-      position = "right",
-      width = 0.22,
-    },
-    interactive = true,
-  })
-end, { desc = "在右侧终端中打开 opencode" })
-
--- 在下方打开单个终端
-map("n", "<leader>ftc", function()
+-- 在下方打开终端（当前文件所在目录）
+map("n", "<leader>ft", function()
+  local dir = vim.fn.expand("%:p:h")
   Snacks.terminal(nil, {
-    cwd = LazyVim.root(),
+    cwd = dir,
     win = {
       position = "bottom",
       height = 0.25,
     },
   })
 end, { desc = "下方终端" })
-
--- 下方左右：终端 + cava
-map("n", "<leader>ftv", function()
-  local root = LazyVim.root()
-  Snacks.terminal(nil, {
-    cwd = root,
-    win = {
-      position = "bottom",
-      height = 0.25,
-    },
-  })
-  vim.cmd("vsplit")
-  vim.cmd("wincmd l")
-  vim.cmd("enew")
-  local chan = vim.fn.termopen(vim.o.shell, { cwd = root })
-  vim.fn.chansend(chan, "cava\n")
-  vim.cmd("stopinsert")
-  vim.api.nvim_win_set_width(0, math.floor(vim.api.nvim_win_get_width(0) * 0.8))
-  vim.cmd("wincmd h")
-end, { desc = "下方终端 + cava" })
-
--- 在浮动终端中打开 opencode
-map("n", "<leader>tO", function()
-  Snacks.terminal("opencode", {
-    win = {
-      position = "float",
-      width = 0.9,
-      height = 0.8,
-    },
-    interactive = true,
-  })
-end, { desc = "在浮动终端中打开 opencode" })
 
 -- 终端模式下 Ctrl+HJKL 跳转窗口
 map("t", "<C-h>", "<C-\\><C-n><C-w>h", { desc = "终端左移" })
