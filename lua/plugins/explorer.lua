4f2289c3ae0975befc0fb7f692527fc65a6c7aba
catos@misser.top <missercatos@users.noreply.github.com>
2026-09-11 14:47:47 +0800

    修复了多语言重叠BUG

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -69,6 +69,8 @@ return {
                       relative = "editor",
                       width = width,
                       height = height,
+                      row = math.floor((vim.o.height - height) / 2),
+                      col = math.floor((vim.o.width - width) / 2),
                       style = "minimal",
                       border = "rounded",
                       title = " 批量创建 (每行一个路径, 结尾 / = 目录, Ctrl-s 确认, q 取消) ",
